a1ed680e577f6f9e0e1b270dd4d0e4b735c1059f
rhancav <edcavdar@gmail.com>
2021-08-31 21:28:43 +0300

    repository annotation

--- school-management/src/main/java/dev/schoolmanagement/repository/CourseRepository.java
+++ school-management/src/main/java/dev/schoolmanagement/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package dev.schoolmanagement.repository;
 
 import dev.schoolmanagement.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
--- school-management/src/main/java/dev/schoolmanagement/repository/ExceptionLogRepository.java
+++ school-management/src/main/java/dev/schoolmanagement/repository/ExceptionLogRepository.java
@@ -3,9 +3,10 @@ package dev.schoolmanagement.repository;
 import dev.schoolmanagement.entity.ExceptionLog;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ExceptionLogRepository extends JpaRepository<ExceptionLog, Long> {
     List<ExceptionLog> findAllByType(String type);
     List<ExceptionLog> findAllByCreatedDate(Local creationDate);
